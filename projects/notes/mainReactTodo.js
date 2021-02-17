837bf38e20c16636b6af7b0ec65ec68e43864250
HelloBro89 <pavelvaloshyn89@gmail.com>
2021-02-17 17:34:38 +0100

    notes

--- projects/notes/mainReactTodo.js
+++ projects/notes/mainReactTodo.js
@@ -1,6 +1,59 @@
 // import logo from './logo.svg';
 import React from 'react';
 
+async function sendAdd(mess) {
+
+    let requestPost = await fetch('/toDoPage/addTask', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(mess)
+    });
+    let result = await requestPost.text();
+    console.log(result)
+};
+
+async function sendChange(mess) {
+
+    let requestPost = await fetch('/toDoPage/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        // body: mess
+        body: JSON.stringify(mess)
+    });
+    let result = await requestPost.text();
+    console.log(result)
+};
+
+async function clearDB() {
+    let requestClear = await fetch('/toDoPage/clear',
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify()
+        });
+    let res = await requestClear.text();
+    console.log(res);
+};
+
+async function delOneTask(mess) {
+    let requestClear = await fetch('/toDoPage/delOneTask',
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(mess)
+        });
+    let res = await requestClear.text();
+    console.log(res);
+}
+
 class DelButton extends React.Component {
 
     constructor(props) {
@@ -20,27 +73,26 @@ class DelButton extends React.Component {
     }
 }
 
-const OneTask = (props) => {
-    return (<input type="checkbox" checked={props.check} />)
-}
+const OneTask = props =>
+    <input type="checkbox" checked={props.check} />;
+
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            title: this.props.data.title,
-            tasks: [],
+            tasks: []
         };
-
         this.add = this.add.bind(this);
         this.clear = this.clear.bind(this);
         this.changeStyle = this.changeStyle.bind(this);
         this.delTask = this.delTask.bind(this);
-    }
+    };
 
     delTask(numDiv) {
         Number(numDiv);
+        delOneTask({ idTask: this.state.tasks[numDiv].time });
         this.state.tasks.splice(numDiv, 1)
         this.setState({ tasks: this.state.tasks })
     }
@@ -48,27 +100,65 @@ class App extends React.Component {
     changeStyle(e) {
         let classIndex = Number(e.target.parentElement.className);
         let textInfo = e.target.parentElement.textContent.slice(1);
+        let timeOfCreation = this.state.tasks[classIndex].time;
+
         let textDecor = (e.target.parentElement.style.textDecoration === 'none') ?
             {
-                task: textInfo, styles: { textDecoration: 'line-through' },
+                task: textInfo,
+                styles: { textDecoration: 'line-through' },
                 checkBoxStatus: true,
-                component: <DelButton filter={this.delTask} />
+                component: <DelButton filter={this.delTask} />,
+                time: timeOfCreation,
+                checkComponent: true
             } :
             {
-                task: textInfo, styles: { textDecoration: "none" }, checkBoxStatus: false,
-                component: null
+                task: textInfo,
+                styles: { textDecoration: "none" },
+                checkBoxStatus: false,
+                component: null, time: timeOfCreation,
+                checkComponent: false
             };
+
+        sendChange({ idTask: timeOfCreation, task: JSON.stringify(textDecor) });
 
         this.state.tasks[classIndex] = textDecor;
         this.setState({ tasks: this.state.tasks })
     }
 
+    async componentDidMount() {
+        let requestGet = await fetch('/toDoPage/tasks');
+        let message = await requestGet.json();
+
+        message = message.map(item => item.task = JSON.parse(item.task));
+
+        for (let sort of message) {
+            if (sort.checkComponent === true) {
+                sort.component = <DelButton filter={this.delTask} />;
+            };
+        };
+
+        this.setState({ tasks: message });
+    }
+
     add() {
+        let timeAddTask = `${new Date().getUTCHours()}:${new Date().getUTCMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}`;
 
         let text = document.getElementById('string').value;
-        this.state.tasks.push({
-            task: text, styles: { textDecoration: "none" },
-            checkBoxStatus: false, component: null
+
+        let obj = {
+            task: text,
+            styles: { textDecoration: "none" },
+            checkBoxStatus: false,
+            component: null,
+            time: timeAddTask,
+            checkComponent: false
+        }
+
+        this.state.tasks.push(obj);
+
+        sendAdd({
+            task: JSON.stringify(obj),
+            idTask: timeAddTask
         });
 
         this.setState({ tasks: this.state.tasks });
@@ -76,16 +166,18 @@ class App extends React.Component {
     }
 
     clear() {
+        clearDB();
         this.setState({ tasks: [] })
     }
 
     render() {
 
         return <div>
-            <p> {this.state.title}</p>
+            <p>My ToDo </p>
             <div onChange={this.changeStyle}>
                 {
                     this.state.tasks.map(function (item, ind) {
+
                         return <div className={ind} style={item.styles} key={ind} > < OneTask check={item.checkBoxStatus} />{item.task}{item.component}</div>
                     })
                 }
@@ -98,5 +190,4 @@ class App extends React.Component {
         </div >
     }
 }
-
 export default App;
